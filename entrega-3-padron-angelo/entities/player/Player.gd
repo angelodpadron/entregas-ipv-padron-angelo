63f58431c893bb558f0751da48e45bb6cb8a86ba
Angelo Padron <padron891@gmail.com>
2024-04-10 16:03:32 -0300

    Desafio 3: Completado

--- entrega-3-padron-angelo/entities/player/Player.gd
+++ entrega-3-padron-angelo/entities/player/Player.gd
@@ -5,12 +5,16 @@ var speed:float = 200
 var cannon:Sprite2D = $Cannon # O(log n)
 var projectile_container:Node
 
+var acceletation:float = 20.0
+var horizontal_speed_limit:float = 600.0
+var friction_weight:float = 0.1 
+var velocity:Vector2 = Vector2.ZERO
+
 func set_projectile_container(container:Node):
 	cannon.projectile_container = container
 	projectile_container = container
 
 func _process(delta):		
-	var direction:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))	
 	var mouse_position:Vector2 = get_global_mouse_position()
 	
 	# Ejemplo calculo de desplazamiento vectorial
@@ -26,6 +30,14 @@ func _process(delta):
 			
 	if Input.is_action_just_pressed("fire"):
 		cannon.fire()
+		
+	# Movimiento y momento
+	var direction:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))	
+	
+	if direction != 0:
+		velocity.x = clamp(velocity.x + (direction * acceletation), -horizontal_speed_limit, horizontal_speed_limit)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, friction_weight) if abs(velocity.x) > 1 else 0
 			
-	position.x += direction * speed * delta
+	position += velocity * delta
 
