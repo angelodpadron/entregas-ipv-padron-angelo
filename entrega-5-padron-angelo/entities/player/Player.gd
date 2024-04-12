a8c71ccda35817cedabaea9cc1094410f284aa8b
Angelo Padron <padron891@gmail.com>
2024-04-12 11:36:38 -0300

    Desafio 5: Agregar logica de colisiones para saltar

--- entrega-5-padron-angelo/entities/player/Player.gd
+++ entrega-5-padron-angelo/entities/player/Player.gd
@@ -1,20 +1,20 @@
-extends Sprite2D
+extends CharacterBody2D
 
 @onready var cannon = $Cannon
 
 @export var ACCELERATION:float = 20.0
 @export var H_SPEED_LIMIT:float = 600.0
 @export var FRICTION_WEIGHT:float = 0.1
+@export var JUMP_SPEED:float = -10.0
+@export var GRAVITY:float = 2.0
 
-var velocity:Vector2 = Vector2.ZERO
 var projectile_container:Node
 
 func initialize(projectile_container):
 	self.projectile_container = projectile_container
 	cannon.projectile_container = projectile_container
-
-func _physics_process(delta):
 	
+func get_input():
 	# Cannon rotation
 	var mouse_position:Vector2 = get_global_mouse_position()
 	cannon.look_at(mouse_position)
@@ -33,5 +33,12 @@ func _physics_process(delta):
 		velocity.x = clamp(velocity.x + (h_movement_direction * ACCELERATION), -H_SPEED_LIMIT, H_SPEED_LIMIT)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
+		
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_SPEED
 	
-	position += velocity * delta
+
+func _physics_process(delta):	
+	get_input()	
+	velocity.y += GRAVITY
+	move_and_slide()
